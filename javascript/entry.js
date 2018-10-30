f5d48a1a73c219aa426fda75f4e79d39de749a31
amanpriya-k <amanpriya.kulkarni@gmail.com>
2018-10-30 13:21:20 -0700

    Begin creating classes

---
+++ javascript/entry.js
@@ -0,0 +1,14 @@
+import Main from './main';
+// const Main = require('./main');
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log('webpack is working!');
+
+  const root = document.getElementById('root');
+  root.innerHTML = 'from entry file';
+
+  const canvasEl = document.getElementById('canvas');
+  const ctx = canvasEl.getContext('2d'); 
+  const main = new Main();
+  main.draw(ctx);
+});
---
+++ javascript/main.js
@@ -0,0 +1,14 @@
+class Main {
+
+  constructor() {
+  }
+
+  draw(ctx) {
+    ctx.clearRect(0, 0, 400, 400);
+    ctx.fillStyle = "#900C3F";
+    ctx.fillRect(0, 0, 400, 400);
+  }
+
+}
+
+export default Main;
