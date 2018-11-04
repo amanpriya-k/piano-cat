8460115ebbfe2b8f40c5a73ce6be386da113f63b
amanpriya-k <amanpriya.kulkarni@gmail.com>
2018-11-03 17:07:04 -0700

    Label keys

--- app/index.js
+++ app/index.js
@@ -8,6 +8,7 @@ import '../assets/images/catty-t.png';
 import '../assets/images/catty.png';
 import '../assets/images/gradient.png';
 import '../assets/images/keyhelper.png';
+import '../assets/images/key_labels.png';
 import '../assets/images/piano-cat.png';
 import '../assets/images/spritesheet.png';
 import '../assets/images/spritesheet1.png';
@@ -23,27 +24,33 @@ const LEVELS = [
       timeouts: [],
       instructions: "How To Play: There are 5 levels of the game! In each level you will learn a basic piano skill. I will play the notes for you, and you can play them back to me to go to the next level!"
   },
-  { number: 1, 
-       demo: [ 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l' ], 
-       notes: [ 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l' ], 
-       timeouts: [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000],
-       instructions: "Welcome! In this first level, you will be playing a simple major scale on the piano. Watch the keys light up and play, and then using your keyboard, play <br> 'C D E F G A B'."
-  },
-  { number: 3,
-       demo: [ 's', 'f', 'h', 's', 'f', 'h'],
-       notes: [ 's', 'f', 'h'],
-       timeouts: [1000, 2000, 3000, 4000, 4000, 4000],
-    instructions: 'Great job! In this level, you will learn to play a C major chord. Play <br> \'C E G \''
-  },
-  { number: 4,
-       demo: ['d', 'y', 'j', 'd', 'y', 'j'],
-       notes: [ 'd', 'y', 'j'],
-       timeouts: [1000, 2000, 3000, 4000, 4000, 4000],
-       instructions: "Awesome! Now I'll show you a D chord. Play the notes <br> 'D F# A."
-  },
+  // { number: 1, 
+  //      demo: [ 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l' ], 
+  //      notes: [ 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l' ], 
+  //      timeouts: [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000],
+  //      instructions: "Welcome! In this first level, you will be playing a simple major scale on the piano. Watch the keys light up and play, and then using your keyboard, play <br> 'C D E F G A B'."
+  // },
+  // { number: 3,
+  //      demo: [ 's', 'f', 'h', 's', 'f', 'h'],
+  //      notes: [ 's', 'f', 'h'],
+  //      timeouts: [1000, 2000, 3000, 4000, 4000, 4000],
+  //   instructions: 'Great job! In this level, you will learn to play a C major chord. Play <br> \'C E G \''
+  // },
+  // { number: 4,
+  //      demo: ['d', 'y', 'j', 'd', 'y', 'j'],
+  //      notes: [ 'd', 'y', 'j'],
+  //      timeouts: [1000, 2000, 3000, 4000, 4000, 4000],
+  //      instructions: "Awesome! Now I'll show you a D chord. Play the notes <br> 'D F# A."
+  // },
   { number: 5,
+       demo: ['s', 's', 'h', 'h', 'j', 'j', 'h', 'g', 'g', 'f', 'f', 'd', 'd', 's'],
+       notes: ['s', 's', 'h', 'h', 'j', 'j', 'h', 'g', 'g', 'f', 'f', 'd', 'd', 's'],
+       timeouts: [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000],
+       instructions: "Nice job! Let's try and play a simple song now. Play the notes <br> 'C C G G A A G' <br> 'F F E E D D C'."
+  },
+  { number: 6,
        demo: ['l', 'k', 'j', 'h', 'g',  'h', 'j', 'l',   'k', 'j', 'h', 'g', 'd'],
-    notes: ['l', 'k', 'j', 'h', 'g', 'h', 'j', 'l', 'k', 'j', 'h', 'g', 'd'],
+       notes: ['l', 'k', 'j', 'h', 'g', 'h', 'j', 'l', 'k', 'j', 'h', 'g', 'd'],
     timeouts: [1000, 1750, 2500, 3250, 3500,    4500, 5000, 5500,   6250, 7000, 7500, 8250, 9000 ],
        instructions: "You're ready to start playing a song! This one's a little harder - follow after me: <br> 'C B A G F'"
   }
@@ -58,8 +65,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const canvasEl = document.getElementById('canvas');
   const canvasEl2 = document.getElementById('canvas2');
+  const canvasEl3 = document.getElementById('canvas3');
   const ctx = canvasEl.getContext('2d');
   const ctx2 = canvasEl2.getContext('2d');
+  const ctx3 = canvasEl3.getContext('2d');
+  let keyLabel = new Image();
+  keyLabel.src = "../assets/images/key_labels.png";
+  // keyLabel.src = '../assets/images/catty.png';
+  // debugger
+  // ctx3.fillStyle = 'red';
+  // ctx3.fillRect(1, 0, 400, 50);
+  keyLabel.onload = () => (
+    ctx3.drawImage(keyLabel, 828, 315, 50, 50, 0, 0, 400, 50)
+  );
 
   const startBtnEl = document.getElementById('start-btn');
   const demoBtnEl = document.getElementById('demo-btn');
